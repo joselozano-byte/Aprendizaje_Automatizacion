4eee3952488d2eab52b0ed7b14417692efc59d9d
Jose Lozano <joselozano@mac.com>
2026-06-07 11:31:21 +0200

    etapa1: ejercicio 1 variables

---
+++ etapa1/01_variables.py
@@ -0,0 +1,10 @@
+nombre = "Jose"
+edad = 61
+trabajadores = 85
+
+print("Nombre:", nombre)
+print("Edad:", edad)
+print("Trabajadores:", trabajadores)
+print(type(nombre))
+print(type(edad))
+print(type(trabajadores))
